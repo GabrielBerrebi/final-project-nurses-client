269f201e907de38b2ef159a51dd5bc57183f355a
Gabriel <gelbaz@g.jct.ac.il>
2022-10-05 22:25:35 +0300

    Add Reset in Login Form

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -3,6 +3,12 @@ import {Button, Card, Form, Input, Space} from 'antd';
 import styles from './Login.module.less'
 
 const Login = () => {
+    const [form] = Form.useForm();
+
+    const resetForm = () => {
+        form.resetFields();
+    }
+
     const submitForm = (values: any) => {
         console.log(values);
     }
@@ -12,21 +18,20 @@ const Login = () => {
             <div className={styles.login}>
                 <Card title='Login' bordered={true} className={styles.card}>
                     <Form name='login' initialValues={{remember: true}} size='large' autoComplete='off'
-                          onFinish={submitForm} className={styles.cardInputs}
+                          onFinish={submitForm} className={styles.cardInputs} form={form}
                           labelCol={{span: 8}} wrapperCol={{span: 16}}>
                         <Form.Item label='Username' name='username'
                                    rules={[{required: true, type: 'email'}]}>
-                            <Input />
+                            <Input/>
                         </Form.Item>
-                        <Space />
+                        <Space/>
                         <Form.Item label='Password' name='password'
                                    rules={[{required: true}]}>
                             <Input.Password/>
                         </Form.Item>
                         <Form.Item wrapperCol={{offset: 8, span: 16}}>
-                            <Button type="primary" htmlType="submit">
-                                Submit
-                            </Button>
+                            <Button type='primary' htmlType='submit' style={{marginRight: 16}}>Submit</Button>
+                            <Button htmlType='reset' onClick={resetForm}>Reset</Button>
                         </Form.Item>
                     </Form>
                 </Card>
